aa1fe07003ef6986b348e6032923cb56436e73ca
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-12 14:11:37 -0400

    added CSS styling

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const IMAGES = [
+    "https://thumbs.gfycat.com/TemptingAngelicHoneybee-small.gif",
+    "http://farm5.static.flickr.com/4063/4362705129_261db3ea0c_o.gif",
+    "https://vignette.wikia.nocookie.net/timanderic/images/3/36/Pumpers.gif/revision/latest?cb=20120815131932"
+];
+
+// Alternate version if you're using images on the hard drive.
+// make sure to copy the images to an "images" folder in your project
+// // const IMAGES = [
+
+// ]
