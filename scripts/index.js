e273e5d75719fb142497f6de10723705e76f1842
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-12 15:34:47 -0400

    fixed a lot of code and bugs.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,34 @@
+// Instaclone!
 const IMAGES = [
-    "https://thumbs.gfycat.com/TemptingAngelicHoneybee-small.gif",
-    "http://farm5.static.flickr.com/4063/4362705129_261db3ea0c_o.gif",
-    "https://vignette.wikia.nocookie.net/timanderic/images/3/36/Pumpers.gif/revision/latest?cb=20120815131932"
+    "https://scontent-sjc3-1.cdninstagram.com/vp/54a8da585c48cc60b0f4cddc6d520550/5C5F929F/t51.2885-15/e35/39356533_2097180177200225_2485126450366119936_n.jpg",
+    "https://scontent-sjc3-1.cdninstagram.com/vp/34f7ade8a4fe3b18c51b784591271ab1/5C60D8C4/t51.2885-15/e35/39309330_497705957372805_1938929854401478656_n.jpg",
+    "https://scontent-sjc3-1.cdninstagram.com/vp/16fc6163cf9026578771dd056c553ee4/5C507411/t51.2885-15/e35/38753626_293030034614624_3652947776343375872_n.jpg"
 ];
 
 // Alternate version if you're using images on the hard drive.
-// make sure to copy the images to an "images" folder in your project
-// // const IMAGES = [
+// Make sure to copy the images to an "images" folder in your project
+// const IMAGES = [
+//     "images/oakley-1.jpg",
+//     "images/milla-666.jpg",
+//     "images/oakley-2.jpg"
+// ];
 
-// ]
+//  function that generates the thumbnail div
+// funcation that generates an img element
+
+function createImage(imageURL){
+    const theImage = document.createElement('img');
+
+    // theImage.src = imageURL;
+    theImage.setAttribute('src', imageURL);
+
+    return theImage;
+}
+
+// function that generates the thumbnail div
+function createThumbnail(url) {
+    const theContainer = document.createElement('div');
+    theContainer.classList.add('thumbnail-item');
+    theContainer.appendChild(createImage(url));
+
+}
