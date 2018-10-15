cdfc2fd07d05b156ea84909c093f8930f7bc7e9e
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-15 12:24:25 -0400

    fixed some javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 // Instaclone!
 const IMAGES = [
-    "https://thumbs.gfycat.com/TemptingAngelicHoneybee-small.gif",
-    "http://farm5.static.flickr.com/4063/4362705129_261db3ea0c_o.gif",
-    "https://static1.fjcdn.com/thumbnails/comments/Thanks+buddy+have+a+random+gif+_fc5c9c6c4c6b447f37d8f54e6c84b3b1.gif",
+    {url: "http://farm5.static.flickr.com/4063/4362705129_261db3ea0c_o.gif", Alt="super amazing photo"},
+    {url: "https://thumbs.gfycat.com/TemptingAngelicHoneybee-small.gif", Alt="super amazing photo 2"},
+    {url: "https://static1.fjcdn.com/thumbnails/comments/Thanks+buddy+have+a+random+gif+_fc5c9c6c4c6b447f37d8f54e6c84b3b1.gif", Alt="super amazing photo 3"}
 ];
 
 // Alternate version if you're using images on the hard drive.
@@ -13,12 +13,17 @@ const IMAGES = [
 //     "images/oakley-2.jpg"
 // ];
 
+const outputElement = document.querySelector('[data-output');
+
 // function that generates an img element
-function createImage(imageURL) {
+function createImage(imageInfo) {
     const theImage = document.createElement('img');
     
-    // theImage.src = imageURL;
-    theImage.setAttribute('src', imageURL);
+    // theImage.src = imageInfo;
+    theImage.setAttribute('src', imageInfo.url);
+    theImage.setAttribute('alt', imageInfo.alt);
+    theImage.setAttribute('title', imageInfo.title);
+
 
     // add an event listener to the image
     theImage.addEventListener('click', function (event) {
@@ -54,10 +59,10 @@ function createThumbnail(imageURL){
 // loop through IMAGES array.
 // For each image, call the anonymous function.
 // The anon func should expect to recieve and image URL
-IMAGES.forEach(function(anImageUrl) {
+IMAGES.forEach(function(singleImageInfo) {
     
     // We pass that image URL to our createThumbnail func
-    let testThumb = createThumbnail(anImageUrl);
+    let testThumb = createThumbnail(singleImageInfo);
 
     // then append that thumnail to the page.
     document.body.appendChild(testThumb);
